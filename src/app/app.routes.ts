d13ee1882654326f45465092d1bdb47a06ca26a8
Rosa Torres <randreafer11abril@gmail.com>
2026-09-14 00:04:55 -0600

    Agregar tabla de recetas con detalle y navegacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
+import { Recipes } from './pages/recipes/recipes';
+import { RecipesDetail } from './pages/recipes-detail/recipes-detail';
+import { RecipesList } from './pages/recipes-list/recipes-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'contact', component: Contact },
+  {path: 'recipes-list', component: RecipesList},
+  {path: 'recipes-detail/:id', component: RecipesDetail},
+  {path: 'recipes', component: Recipes},
+];
